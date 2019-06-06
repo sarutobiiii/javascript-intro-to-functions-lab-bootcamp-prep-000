6e67cab742b394be972f48f53be96653a6c347ba
oluwatobi balogun <tobibags19@gmail.com>
2019-06-06 00:29:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,8 +15,7 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string == string.toLowerCase()) {
-    console.log("I can't hear you!")
+  if (string == string.toLowerCase()) return "I can't hear you!"
   } else if (string == string.toUpperCase()) {
     console.log("YES INDEED!")
   }
