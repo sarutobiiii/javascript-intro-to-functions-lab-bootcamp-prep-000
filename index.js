fbaba2bf20cb411da812a55d9ec5b0b9fe95c978
oluwatobi balogun <tobibags19@gmail.com>
2019-06-06 00:27:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,5 +20,7 @@ function sayHiToGrandma(string) {
   } else if (string == string.toUpperCase()) {
     console.log("YES INDEED!")
   }
-  else if (string == "I love you, grandma")
+  else if (string == "I love you, Grandma.") {
+    console.log("I love you, too.")
+  }
 }
