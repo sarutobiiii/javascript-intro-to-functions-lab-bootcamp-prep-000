beec92f71d013e61d9998329b0baeff8e539599c
oluwatobi balogun <tobibags19@gmail.com>
2019-06-06 00:30:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,11 +15,7 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string == string.toLowerCase()) return "I can't hear you!";
-  } else if (string == string.toUpperCase()) {
-    console.log("YES INDEED!")
-  }
-  else if (string == "I love you, Grandma.") {
-    console.log("I love you, too.")
-  }
+  if (string === string.toLowerCase()) return "I can't hear you!";
+  else if (string == string.toUpperCase()) return "YES INDEED!";
+  else if (string == "I love you, Grandma.") return "I love you, too.";
 }
